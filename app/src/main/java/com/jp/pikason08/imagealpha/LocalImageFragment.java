4459873fa15812e3c6fc7bf6eada8e6031405313
Keigo <keigo@vpn114.vpn.kanazawa-it.ac.jp>
2017-01-17 18:40:50 +0900

    add recyclerview action and layout

--- app/src/main/java/com/jp/pikason08/imagealpha/LocalImageFragment.java
+++ app/src/main/java/com/jp/pikason08/imagealpha/LocalImageFragment.java
@@ -3,9 +3,16 @@ package com.jp.pikason08.imagealpha;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * Created by keigo on 2017/01/14.
@@ -13,15 +20,40 @@ import android.view.ViewGroup;
 
 public class LocalImageFragment extends Fragment{
 
+    private RecyclerView recyclerView;
+    private List<LocalData> localData = new ArrayList<>();
+
+    protected final static int COLUMN_NUM = 3;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        createDummy();
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_local_image, null);
+        initViews(view);
         return view;
+    }
+
+    private void initViews(View view){
+        recyclerView = (RecyclerView)view.findViewById(R.id.recycler_view_local);
+        LocalRecyclerAdapter adapter = new LocalRecyclerAdapter(getContext(), localData);
+        RecyclerView.LayoutManager manager = new GridLayoutManager(getActivity(), COLUMN_NUM);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void createDummy(){
+        for (int i = 0;i < 15;i++){
+            LocalData data = new LocalData();
+            data.setId(i);
+            data.setDate("201"+i+"/xx/yy");
+            data.setLocalImage(null);
+            localData.add(data);
+        }
     }
 }
